e9c18e67d31c671f450dccc307a3264a09cf089b
MikoloD <grygiel.mikolaj@gmail.com>
2023-04-27 13:18:00 +0200

    SessionService

---
+++ RedisBasket/Services/SessionService.cs
@@ -0,0 +1,16 @@
+﻿namespace RedisBasket.Services
+{
+	public class SessionService
+	{
+        public string? SessionId { get; set; }
+		private readonly IHttpContextAccessor _httpContext;
+        public SessionService(IHttpContextAccessor httpContext)
+        {
+			_httpContext = httpContext;
+		}
+		public void Run()
+		{
+			SessionId = _httpContext.HttpContext.Session.Id;
+		}
+    }
+}
