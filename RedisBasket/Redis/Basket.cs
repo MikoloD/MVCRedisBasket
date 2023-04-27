78c362b028f412399e88b550c3d40eccf6a5c41f
MikoloD <grygiel.mikolaj@gmail.com>
2023-04-27 13:17:18 +0200

    Create Basket.cs

---
+++ RedisBasket/Redis/Basket.cs
@@ -0,0 +1,20 @@
+﻿using Redis.OM.Modeling;
+using System.Net;
+
+namespace RedisBasket.Redis
+{
+    [Document(StorageType = StorageType.Json, Prefixes = new[] { "Basket" })]
+    public class Basket
+    {
+        [RedisIdField]
+        [Indexed]
+        public string? SessionId { get; set; }
+
+        [Indexed(CascadeDepth = 1)]
+        public List<Product> BasketPosition { get; set; } = new List<Product>();
+
+        [Indexed]
+        public decimal? CheckoutPrice { get; set; } = 0;
+
+    }
+}
