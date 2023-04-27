b005c530d22859132bf13f490a4bab3479abd6c1
MikoloD <grygiel.mikolaj@gmail.com>
2023-04-27 13:17:08 +0200

    Create BasketViewModel.cs

---
+++ RedisBasket/Models/BasketViewModel.cs
@@ -0,0 +1,10 @@
+﻿using RedisBasket.Redis;
+
+namespace RedisBasket.Models
+{
+    public class BasketViewModel
+    {
+        public Basket Basket { get; set; }
+        public Product AddProduct { get; set; }
+    }
+}
