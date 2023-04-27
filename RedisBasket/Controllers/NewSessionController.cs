b363b4116039a37ba5a990c6ddacbf1771ef60d6
MikoloD <grygiel.mikolaj@gmail.com>
2023-04-27 13:17:04 +0200

    Create NewSessionController.cs

---
+++ RedisBasket/Controllers/NewSessionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisBasket.Services;
+
+namespace RedisBasket.Controllers
+{
+	public class NewSessionController : Controller
+	{
+		private SessionService _sessionService;
+		public NewSessionController(SessionService SessionService)
+        {
+			_sessionService = SessionService;
+		}
+        public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Create()
+		{
+			_sessionService.Run();
+			return RedirectToAction(nameof(Index));
+		}
+	}
+}
