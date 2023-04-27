f92e5175d24d3c2119a81ff9e364159333ab417d
MikoloD <grygiel.mikolaj@gmail.com>
2023-04-27 13:17:11 +0200

    Update Program.cs

--- RedisBasket/Program.cs
+++ RedisBasket/Program.cs
@@ -1,7 +1,20 @@
+using Redis.OM;
+using RedisBasket.Redis;
+using RedisBasket.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var redisConnectionString = "redis://localhost:6379";
+builder.Services.AddSingleton(new RedisConnectionProvider(redisConnectionString));
+builder.Services.AddHostedService<IndexCreationService>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession(options =>
+{
+	options.IdleTimeout = TimeSpan.FromMinutes(30);
+});
+builder.Services.AddSingleton<SessionService>();
 
 var app = builder.Build();
 
@@ -19,5 +32,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseSession();
 
 app.Run();
