18501fb43c25c7f43bfac00069cd2be3e8b48648
MikoloD <grygiel.mikolaj@gmail.com>
2023-04-27 13:17:28 +0200

    Create Product.cs

---
+++ RedisBasket/Redis/Product.cs
@@ -0,0 +1,24 @@
+﻿using Redis.OM.Modeling;
+using System.ComponentModel.DataAnnotations;
+
+namespace RedisBasket.Redis
+{
+    public class Product
+    {
+        [Indexed]
+		[Required(ErrorMessage = "Please enter product id")]
+		public int? ProductId { get; set; }
+
+		[Required(ErrorMessage = "Please enter product name")]
+		[Indexed]
+        public string? ProductName { get; set; }
+
+		[Required(ErrorMessage = "Please enter product quantity")]
+		[Indexed]
+        public int? Quantity { get; set; }
+
+		[Required(ErrorMessage = "Please enter product price")]
+		[Indexed]
+        public decimal? Price { get; set; }
+    }
+}
